152929cfa7f85446d4cb83f56a26f80fa6a71e58
crille <christopher.ljungblad@gmail.com>
2018-11-13 22:59:06 +0100

    First commit - clean spring boot, only spring core project

---
+++ src/main/java/se/cljungblad/springcoretraining/SpringCoreTrainingApplication.java
@@ -0,0 +1,12 @@
+package se.cljungblad.springcoretraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreTrainingApplication.class, args);
+	}
+}
---
+++ src/test/java/se/cljungblad/springcoretraining/SpringCoreTrainingApplicationTests.java
@@ -0,0 +1,16 @@
+package se.cljungblad.springcoretraining;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCoreTrainingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
