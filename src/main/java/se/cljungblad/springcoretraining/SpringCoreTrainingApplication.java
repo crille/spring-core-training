0626e7b35a21d5462ea6677aefdb19d7ba7b964b
crille <christopher.ljungblad@gmail.com>
2018-11-14 21:03:39 +0100

    Tutorial 1 - Application context. Create a bean and play with lifecycle for scope singleton.

--- src/main/java/se/cljungblad/springcoretraining/SpringCoreTrainingApplication.java
+++ src/main/java/se/cljungblad/springcoretraining/SpringCoreTrainingApplication.java
@@ -3,6 +3,7 @@ package se.cljungblad.springcoretraining;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import se.cljungblad.springcoretraining.services.GameService;
 
 @SpringBootApplication
 public class SpringCoreTrainingApplication {
@@ -15,5 +16,15 @@ public class SpringCoreTrainingApplication {
 		for(String beanName : applicationContext.getBeanDefinitionNames()) {
 			System.out.println("Bean name: " + beanName);
 		}
+
+		System.out.println("Is gameService a singleton? " + applicationContext.isSingleton("gameServiceImpl"));
+		GameService gameService = applicationContext.getBean(GameService.class);
+
+		System.out.println(gameService.getGameNames());
+
+		GameService gameService2 = applicationContext.getBean(GameService.class);
+
+		System.out.println("Is gameServer1 and gameServer2 the same? " + gameService.equals(gameService2));
+		System.out.println(gameService2.getGameNames());
 	}
 }
---
+++ src/main/java/se/cljungblad/springcoretraining/services/GameService.java
@@ -0,0 +1,7 @@
+package se.cljungblad.springcoretraining.services;
+
+import java.util.List;
+
+public interface GameService {
+    List<String> getGameNames();
+}
