ea0f4b0a28d7ec8a02ff32c52c80131e245ae639
crille <christopher.ljungblad@gmail.com>
2018-11-14 19:17:02 +0100

    Tutorial 1 - Application context. Get application context and print the names of all beans it's holding.

--- src/main/java/se/cljungblad/springcoretraining/SpringCoreTrainingApplication.java
+++ src/main/java/se/cljungblad/springcoretraining/SpringCoreTrainingApplication.java
@@ -2,11 +2,18 @@ package se.cljungblad.springcoretraining;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringCoreTrainingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCoreTrainingApplication.class, args);
+
+		ApplicationContext applicationContext = SpringApplication.run(SpringCoreTrainingApplication.class, args);
+
+		System.out.println("Number of beans in application context: " + applicationContext.getBeanDefinitionCount());
+		for(String beanName : applicationContext.getBeanDefinitionNames()) {
+			System.out.println("Bean name: " + beanName);
+		}
 	}
 }
